0ea55c79716c8708b8d19c08db5e29c30b07b494
Cristhian <alfredogarzon1234@hotmail.com>
2025-05-05 10:11:29 -0500

    Sec-9 antes submodulos

--- src/common/config/envs.ts
+++ src/common/config/envs.ts
@@ -6,15 +6,15 @@ import * as joi from 'joi';
 interface EnvvVars {
 
     PORT : number
-    PRODUCTS_MICROSERVICE_HOST : string
-    PRODUCTS_MICROSERVICE_PORT : number
+    // PRODUCTS_MICROSERVICE_HOST : string
+    // PRODUCTS_MICROSERVICE_PORT : number
     NATS_SERVERS : string[]
 }
 
 const envSchema = joi.object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE_HOST : joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT : joi.number().required(),
+    // PRODUCTS_MICROSERVICE_HOST : joi.string().required(),
+    // PRODUCTS_MICROSERVICE_PORT : joi.number().required(),
     NATS_SERVERS : joi.array().items(joi.string()).required()
 })
 .unknown(true)
@@ -31,7 +31,7 @@ if(error){
 const envVars : EnvvVars = value
 export const envs = {
     port : envVars.PORT,
-    productServiceHost : envVars.PRODUCTS_MICROSERVICE_HOST,
-    productServicePort : envVars.PRODUCTS_MICROSERVICE_PORT,
+    // productServiceHost : envVars.PRODUCTS_MICROSERVICE_HOST,
+    // productServicePort : envVars.PRODUCTS_MICROSERVICE_PORT,
     natsServers : envVars.NATS_SERVERS
 }
